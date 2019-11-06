b6efd534b62722893f451bcb3ccd25bb05bd0d0e
Nelson Mendes <nelson.mendes@farfetch.com>
2019-11-06 23:39:39 +0000

    added new Text box

--- mi_card_flutter/build/frontend_server.d
+++ mi_card_flutter/build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /Users/nelsonmendes/Development/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
+frontend_server.d: /Users/nelson.mendes/Developer/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -39,6 +39,52 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
                 ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                        Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+31671276121',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'nelson@mnelson.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),

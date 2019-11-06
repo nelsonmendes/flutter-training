2dbb68ca127980d88cb1d32abb8a4ffb5864d3b9
Nelson Mendes <nelson.mendes@farfetch.com>
2019-11-06 23:54:46 +0000

    New Alignment

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -39,51 +40,41 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
                 ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                        Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '+31671276121',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontSize: 20.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                          Icons.phone,
+                        color: Colors.teal,
                       ),
-                    )
-                  ],
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+31671276121',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
+                  child: ListTile(
+                    leading: Icon(
                       Icons.email,
                       color: Colors.teal,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      'nelson@mnelson.com',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontSize: 20.0,
-                      ),
-                    )
-                  ],
-                ),
+                    title: Text('nelson@mnelson.com'),
+                  )
               )
             ],
           ),
